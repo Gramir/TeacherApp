5c84f6fbcbcac94232b4502d456dff7e8461cd6b
Gilbert <gilberto.ramirez.suazo@gmail.com>
2024-07-27 15:10:02 -0400

    Fix StudentListFragment to show correctly the students

--- app/src/main/java/com/example/teacherapp/ui/student/StudentListFragment.kt
+++ app/src/main/java/com/example/teacherapp/ui/student/StudentListFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.teacherapp.data.database.AppDatabase
 import com.example.teacherapp.data.repository.StudentRepository
@@ -21,6 +20,7 @@ class StudentListFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var viewModel: StudentViewModel
+    private lateinit var adapter: StudentAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentStudentListBinding.inflate(inflater, container, false)
@@ -30,16 +30,36 @@ class StudentListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Log.d("StudentListFragment", "onViewCreated called")
-        Log.d("StudentListFragment", "Arguments: $arguments")
-
-        val courseId = arguments?.getInt("courseId", -1) ?: -1
-        Log.d("StudentListFragment", "Received courseId: $courseId")
-
+        val courseId = arguments?.getInt("courseId") ?: -1
         if (courseId == -1) {
             Log.e("StudentListFragment", "Invalid courseId received")
             Toast.makeText(context, "Error: Invalid course ID", Toast.LENGTH_LONG).show()
             return
+        }
+
+        setupViewModel(courseId)
+        setupRecyclerView()
+        observeStudents()
+    }
+
+    private fun setupViewModel(courseId: Int) {
+        val database = AppDatabase.getDatabase(requireContext())
+        val repository = StudentRepository(database.studentDao())
+        val factory = StudentViewModelFactory(repository)
+        viewModel = ViewModelProvider(this, factory)[StudentViewModel::class.java]
+        viewModel.getStudentsForCourse(courseId)
+    }
+
+    private fun setupRecyclerView() {
+        adapter = StudentAdapter()
+        binding.studentRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.studentRecyclerView.adapter = adapter
+    }
+
+    private fun observeStudents() {
+        viewModel.students.observe(viewLifecycleOwner) { students ->
+            Log.d("StudentListFragment", "Received ${students.size} students")
+            adapter.submitList(students)
         }
     }
 
